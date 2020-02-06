f75a5c2fb0a4126865ebc7e5f07cf55141ab87c7
Chikidik <33221343+Chikidik@users.noreply.github.com>
2020-02-06 08:14:27 +0100

    Update ExerciseOOPjava4.java

--- Homework/ExerciseOOPjava4.java
+++ Homework/ExerciseOOPjava4.java
@@ -2,20 +2,32 @@ import java.util.Random;
 import java.util.Scanner;
 public class ExerciseOOPjava4{
     public static void main(String[] args) {
-    Scanner userInput = new Scanner(System.in);
     Random random = new Random();
-    int [] RanNum = new int[100];
-    int test = userInput.nextInt();
-    
-    if (test > 99||test <0){
-        System.out.println("Out of bounds");
+    int [] ran = new int[100];
+    Scanner userInput = new Scanner(System.in);
+
+    // Filling the array with randomn numbers
+    for (int i = 0; i<ran.length;i++){
+        ran[i] = random.nextInt(100);
+        }
+
+    // counter if you want to do certain number of cycles
+    int counter = 10;
+
+    while(counter!=0){
+
+        // Get User Input
+        int test = userInput.nextInt();
+        if (test > 99||test <0){
+            System.out.println("Out of bounds");
+        }
+
+        // Printing the randomn number
+        System.out.println("The random number is " + ran[test]);
+
+        counter--;
     }
 
-    for (int i = 0; i<RanNum.length;i++){
-        RanNum[i] = random.nextInt(50);
-        //System.out.println(RanNum[i]);
-        }
-        System.out.println("The entry number is " + RanNum[test]);
-        userInput.close();
+    userInput.close();
     }
 }
