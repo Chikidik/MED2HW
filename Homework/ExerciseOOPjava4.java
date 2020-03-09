39360ae0af88563ea3b5b59d45dc4136f4cca740
Chikidik <33221343+Chikidik@users.noreply.github.com>
2020-03-09 02:34:54 +0100

    Add files via upload

--- Homework/ExerciseOOPjava4.java
+++ Homework/ExerciseOOPjava4.java
@@ -1,15 +1,9 @@
-/*
-@Chi Khoa Martin Nguyen
-https://github.com/Chikidik
-TO DO:
-Find a way to close while loop other than iterations.
-
-*/
 import java.util.Random;
 import java.util.Scanner;
 public class ExerciseOOPjava4{
     public static void main(String[] args) {
     Random random = new Random();
+
     int [] ran = new int[100];
     Scanner userInput = new Scanner(System.in);
 
@@ -17,24 +11,26 @@ public class ExerciseOOPjava4{
     for (int i = 0; i<ran.length;i++){
         ran[i] = random.nextInt(100);
         }
+    
 
     // counter if you want to do certain number of cycles
-    int counter = 10;
+    int counter = 100;
 
     while(counter!=0){
 
         // Get User Input
         int test = userInput.nextInt();
-        if (test > 99||test <0){
+        int input = test-1;
+        if (input > 100||input <0){
             System.out.println("Out of bounds");
-        }
+        }else{
 
+        
         // Printing the randomn number
-        System.out.println("The random number is " + ran[test]);
-
+        System.out.println("The random number is " + ran[input]);
         counter--;
+        }
     }
-
     userInput.close();
     }
 }
